77f1f9f5837ed7fb87ba62252e5362d7de4c008d
navassherif98 <navassherif189@gmail.com>
2021-06-11 21:50:06 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,286 @@
+/*
+ * Dew Templates (Personal Portofolio)
+ * Created by : vavelo
+ * Version : 1.1
+ */
+
+
+/* INDEX OF CONTENTS JAVASCRIPT
+==================================================
+  XX. PRELOADER
+  01. NAVIGATION
+  02. MOBILE NAVIGATION
+  03. PARALLAX BACKGROUND EFFECT
+  04. TEXT ROTATE
+  05. HOME ARROW DOWN
+  06. SMOTH SCROLL
+  07. PORTFOLIO FILTER IMAGE
+  08. TESTIMONIAL SLIDER
+  09. ANIMATION EFFECT  
+*/
+
+
+(function ($) {
+	'use strict';
+
+	jQuery(document).ready(function () {
+
+
+		/* XX. PRELOADER
+		==================================================*/
+
+		$(window).on('load', function () {
+			$("#status").fadeOut();
+			$("#preloader").delay(500).fadeOut("slow");
+		});
+
+
+		/* 01. NAVIGATION
+		==================================================*/
+
+		$(window).on('scroll', function () {
+			if ($(window).scrollTop() > 100) {
+				$('#navigation').addClass('nav-bg');
+			} else {
+				$('#navigation').removeClass('nav-bg');
+			}
+		});
+
+
+		/* 02. MOBILE NAVIGATION
+		==================================================*/
+
+		$(document).on('click', '.navbar-collapse.in', function (e) {
+			if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+				$(this).collapse('hide');
+			}
+		});
+
+		$('body').scrollspy({
+			target: '.navbar-collapse',
+			offset: 195
+		});
+
+
+		/* 03. PARALLAX BACKGROUND EFFECT
+		==================================================*/
+
+		var parallax = function () {
+			$(window).stellar();
+		};
+
+		$(function () {
+			parallax();
+		});
+
+
+		/* 04. TEXT ROTATE
+		==================================================*/
+		$(document).on('ready', function () {
+			$('#text-rotator').rotatetext({
+				fadeSpeed: 500,
+				pauseSpeed: 2000
+			});
+		});
+
+
+		/* 05. HOME ARROW DOWN
+		==================================================*/
+
+		var arrowBounce = function () {
+			var arrow = $(".arrow");
+			if (arrow.hasClass("lift")) {
+				arrow.removeClass("lift");
+			} else {
+				arrow.addClass("lift");
+			}
+		};
+
+		setInterval(arrowBounce, 800);
+
+
+		/* 06. SMOTH SCROLL
+		==================================================*/
+
+		$('a.smoth-scroll').on('click', function (e) {
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top - 50
+			}, 1000);
+			e.preventDefault();
+		});
+
+
+		/* 07. PORTFOLIO FILTER IMAGE
+		==================================================*/
+		$('#port-image').mixItUp();
+
+
+		/* 08. TESTIMONIAL SLIDER
+		==================================================*/
+
+		$('.flexslider').flexslider({
+			animation: "fade",
+			directionNav: false
+		});
+
+
+		/* 09. ANIMATION EFFECT
+		==================================================*/
+
+		AOS.init({
+			duration: 1200,
+			once: true,
+			disable: 'mobile'
+		});
+
+		/* 10. CONTACT VALIDATION FORM
+		==================================================*/
+
+		$(function () {
+			$('#contact-form').validate({
+				rules: {
+					name: {
+						required: true,
+						minlength: 2
+					},
+					email: {
+						required: true
+					},
+					message: {
+						required: true
+					}
+				},
+				messages: {
+					name: {
+						required: "Please Input Your Name",
+						minlength: "your name must consist of at least 2 characters"
+					},
+					email: {
+						required: "Please Input Your Email"
+					},
+					message: {
+						required: "Your Message Required"
+					}
+				},
+				submitHandler: function (form) {
+					$(form).ajaxSubmit({
+						type: "POST",
+						data: $(form).serialize(),
+						url: "php/process.php",
+						success: function () {
+							$('#contact :input').attr('disabled', 'disabled');
+							$('#contact').fadeTo("slow", 1, function () {
+								$(this).find(':input').attr('disabled', 'disabled');
+								$(this).find('label').css('cursor', 'default');
+								$('#success').fadeIn();
+							});
+						},
+						error: function () {
+							$('#contact').fadeTo("slow", 1, function () {
+								$('#error').fadeIn();
+							});
+						}
+					});
+				}
+			});
+		});
+
+
+		window.jssor_1_slider_init = function() {
+
+            var jssor_1_SlideshowTransitions = [
+              {$Duration:800,x:0.3,$During:{$Left:[0.3,0.7]},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,x:-0.3,$SlideOut:true,$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,x:-0.3,$During:{$Left:[0.3,0.7]},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,x:0.3,$SlideOut:true,$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,y:0.3,$During:{$Top:[0.3,0.7]},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,y:-0.3,$SlideOut:true,$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,y:-0.3,$During:{$Top:[0.3,0.7]},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,y:0.3,$SlideOut:true,$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,x:0.3,$Cols:2,$During:{$Left:[0.3,0.7]},$ChessMode:{$Column:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,x:0.3,$Cols:2,$SlideOut:true,$ChessMode:{$Column:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,y:0.3,$Rows:2,$During:{$Top:[0.3,0.7]},$ChessMode:{$Row:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,y:0.3,$Rows:2,$SlideOut:true,$ChessMode:{$Row:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,y:0.3,$Cols:2,$During:{$Top:[0.3,0.7]},$ChessMode:{$Column:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,y:-0.3,$Cols:2,$SlideOut:true,$ChessMode:{$Column:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,x:0.3,$Rows:2,$During:{$Left:[0.3,0.7]},$ChessMode:{$Row:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,x:-0.3,$Rows:2,$SlideOut:true,$ChessMode:{$Row:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,x:0.3,y:0.3,$Cols:2,$Rows:2,$During:{$Left:[0.3,0.7],$Top:[0.3,0.7]},$ChessMode:{$Column:3,$Row:12},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,x:0.3,y:0.3,$Cols:2,$Rows:2,$During:{$Left:[0.3,0.7],$Top:[0.3,0.7]},$SlideOut:true,$ChessMode:{$Column:3,$Row:12},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,$Delay:20,$Clip:3,$Assembly:260,$Easing:{$Clip:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,$Delay:20,$Clip:3,$SlideOut:true,$Assembly:260,$Easing:{$Clip:$Jease$.$OutCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,$Delay:20,$Clip:12,$Assembly:260,$Easing:{$Clip:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:800,$Delay:20,$Clip:12,$SlideOut:true,$Assembly:260,$Easing:{$Clip:$Jease$.$OutCubic,$Opacity:$Jease$.$Linear},$Opacity:2}
+            ];
+
+            var jssor_1_options = {
+              $AutoPlay: 1,
+              $SlideshowOptions: {
+                $Class: $JssorSlideshowRunner$,
+                $Transitions: jssor_1_SlideshowTransitions,
+                $TransitionsOrder: 1
+              },
+              $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$
+              },
+              $ThumbnailNavigatorOptions: {
+                $Class: $JssorThumbnailNavigator$,
+                $SpacingX: 5,
+                $SpacingY: 5
+              }
+            };
+
+            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+            /*#region responsive code begin*/
+
+            var MAX_WIDTH = 980;
+
+            function ScaleSlider() {
+                var containerElement = jssor_1_slider.$Elmt.parentNode;
+                var containerWidth = containerElement.clientWidth;
+
+                if (containerWidth) {
+
+                    var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+                    jssor_1_slider.$ScaleWidth(expectedWidth);
+                }
+                else {
+                    window.setTimeout(ScaleSlider, 30);
+                }
+            }
+
+            ScaleSlider();
+
+            $Jssor$.$AddEvent(window, "load", ScaleSlider);
+            $Jssor$.$AddEvent(window, "resize", ScaleSlider);
+            $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+            /*#endregion responsive code end*/
+        };
+
+	});
+
+})(jQuery);
+
+var form = document.getElementById("contact-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("contact-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset()
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
